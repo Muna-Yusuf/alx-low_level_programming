31a4fe17d156d7e318db5bc112543b9a944635c4
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-10 14:09:59 -0700

    RRRRGFGF

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	double ll;
 	float f;
 
-	printf("Size of char: %ld byte(s)\n", sizeof(c));
-	printf("Size of int: %ld bytes(s)\n", sizeof(i));
-	printf("Size of long int : %ld bytes(s)\n", sizeof(l));
-	printf("Size of long long : %ld bytes(s)\n", sizeof(ll));
-	printf("Size of float: %ld bytes(s)\n", sizeof(f));
+	printf("Size of char: %lu byte(s)\n", sizeof(c));
+	printf("Size of int: %lu bytes(s)\n", sizeof(i));
+	printf("Size of long int : %lu bytes(s)\n", sizeof(l));
+	printf("Size of long long : %lu bytes(s)\n", sizeof(ll));
+	printf("Size of float: %lu bytes(s)\n", sizeof(f));
 	return (0);
 }
