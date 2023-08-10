cacad4658cc1f98fec44bf1950e49a634194979c
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-10 11:41:47 -0700

    the 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main -to print somthing
  * Return:zero 
  */
-int main (void)
+int main(void)
 {
-
- puts("Programming is like building a multilingual puzzle");
- 
-	
- return 0;
+ puts("\"Programming is like building a multilingual puzzle");	
+ return (0);
 }
