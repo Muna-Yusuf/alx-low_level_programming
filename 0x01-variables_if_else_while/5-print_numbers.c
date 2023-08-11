2059fbd0419b755182626aefd9bfad75986674a5
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-11 11:29:49 -0700

    the5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main-print numbers 10
+ * Return:0(if done)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
