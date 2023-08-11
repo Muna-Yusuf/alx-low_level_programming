f91d2ca57f99fbc9058236e09cba3baff82a15b7
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-11 10:20:57 -0700

    last dig

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main-random number and last digit
+ * Return:0(if done)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
