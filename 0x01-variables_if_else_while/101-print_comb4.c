db718de96d10f5234347dfc9f005050b8956a2be
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-14 09:44:36 -0700

    the10

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,27 +5,27 @@
  */
 int main(void)
 {
-        int i ;
-        int j ;
-        int x ;
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {  
-             for (x = j + 1 ; x<= 9 ; x++)
-             {
-            putchar(i + '0');
-            putchar(j + '0');
-            putchar(x + '0');
+	int i;
+	int j;
+	int x;
 
-            if (i < 8 || j < 9 || x < 7)
-            {
-            putchar(',');
-            putchar(' ');
-            }
-        }
-    }
-  } 
-    putchar('\n');
-    return (0);
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			for (x = j + 1 ; x <= 9 ; x++)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(x + '0');
+				if (i < 7 || j < 8 || x < 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
