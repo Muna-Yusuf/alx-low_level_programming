1112144f2f80bc46450f7bacf636e2e050d6c621
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-14 23:34:33 -0700

    the3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * alphabet -check for lowercase
+ * Return:0 ( zero if doen)
+ */
+
+int alphabet(void)
+{
+	int i;
+
+	if (i >= 'a' && i <= 'z')
+		return (1);
+	else
+		return (0);
+}
