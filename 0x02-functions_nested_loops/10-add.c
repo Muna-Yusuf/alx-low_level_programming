928a0f204ffcd9ee68799b5973c3db6fae806620
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-15 12:23:36 -0700

    the10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @i: to be added
+ * @j: to be added
+ * Return: sum
+ */
+int add(int i, int j)
+{
+	int c;
+
+	c = i + j;
+
+	return (c);
+}
+
