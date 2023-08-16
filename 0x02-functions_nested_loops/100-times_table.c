1ff0363ff7884775eca6588967f4439b2f3a348b
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-16 11:28:54 -0700

    the12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+#include <stdio.h>
+/**
+ * print_times_table - pritn time table
+ * @n: input
+ * Return: void
+*/
+#include <stdio.h>
+void print_times_table(int n)
+{
+	if ((n >= 0) && (n < 15))
+	{
+		int i, j;
+
+		for (i = 0; i <= n; i++)
+		{
+			printf("0");
+			for (j = 1; j <= n; j++)
+			{
+				if ((i * j) > 9 && (i * j) <= 99)
+					printf(",  %d", (i * j));
+				else if ((i * j) > 99)
+					printf(", %d", (i * j));
+				else
+					printf(",   %d", (i * j));
+			}
+			printf("\n");
+		}
+	}
+}
