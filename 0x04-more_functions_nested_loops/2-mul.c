ede3b1378132d6668c99466530b913b93e3a06c6
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-17 09:55:50 -0700

    the2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: int the frist num
+ * @b: int the second num
+ * Return:multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
