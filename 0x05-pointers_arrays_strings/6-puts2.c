0276dec79557a3423132ee47310acc8dc030c11a
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-21 09:57:31 -0700

    the6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - function that prints every other character of a string
+ * @str: for char
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] ; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
