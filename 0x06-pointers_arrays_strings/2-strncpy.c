d7bf07d31db8f101eb37c1429fb1f8f6a24caa30
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-23 11:36:08 -0700

    the2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,17 +12,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i=0;
 
-        for (i = 0 ; src[i] != '\0';i++)
+        for (i = 0 ; i < n && src[i] != '\0';i++)
         {
-		if (i < n)
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
 	}
-	if (i > n)
+	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
         return (dest);
-        
 }
