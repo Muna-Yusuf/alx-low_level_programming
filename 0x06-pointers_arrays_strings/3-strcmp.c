a75b21a42047b41ed983d0216646ab8471597cb9
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-23 12:11:49 -0700

    the 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,16 @@
 int _strcmp(char *s1, char *s2)
 {
 	int re = 0;
-	int i;
 
-	for (i = 0 ; s1[i] != '\0' ; i++)
+	while (*s1)
 	{
 		if (*s1 != *s2)
 		{
 			re = ((int)*s1 - 48) - ((int)*s2 - 48);
 			break;
 		}
+		s1++;
+		s2++;
 	}
 	return (re);
 }
