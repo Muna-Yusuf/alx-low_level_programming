54dd896e7ef4ccb241da962d06fe038fc8749616
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-24 10:29:50 -0700

    the6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * *cap_string - function that capitalizes all words of a string.
@@ -11,15 +12,25 @@ char *cap_string(char *str)
 	char *str2 = str;
 	int j = 0;
 
-
 	while (str[j])
 	{
 		while (!(str[j] >= 'a' && str[j] <= 'z'))
 		{
 			j++;
 		}
-		if (str[j - 1] == ' ' || str[j - 1] == '\t' || str[j - 1] == '\n' || str[j - 1] == ';' || str[j - 1] == ',' || str[j - 1] == '.' || str[j - 1] == '!' || str[j - 1] == '?' || str[j - 1] == '"' || str[j - 1] == '(' || str[j - 1] == ')' || str[j - 1] == '{' || str[j - 1] == '}' || j == 0)
-			str[j] -= 32;
+		if (str[j - 1] == '\t' ||
+		str[j - 1] == '\n' ||
+		str[j - 1] == ';' ||
+		str[j - 1] == ',' ||
+		str[j - 1] == '.' ||
+		str[j - 1] == '!' ||
+		str[j - 1] == '?' ||
+		str[j - 1] == '"' ||
+		str[j - 1] == '(' ||
+
+		str[j - 1] == ')' || str[j - 1] == '{' ||
+		str[j - 1] == '}' || j == 0)
+		str[j] -= 32;
 		j++;
 	}
 	return (str2);
