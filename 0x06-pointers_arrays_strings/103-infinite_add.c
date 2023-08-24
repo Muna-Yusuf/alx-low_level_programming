7fb00053b7fd0da3a172ea697f23b8a4e733e059
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-24 12:07:53 -0700

    the11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,61 +15,34 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int a = 0, f, s;
 
 	while (n1[a] != '\0')
-	{
-		a++;
-	}
+	{a++; }
 	while (n2[b] != '\0')
-	{
-		b++;
-	}
+	{b++; }
 	if (a > b)
-	{
-		l = a;
-	}
+	{l = a; }
 	else
-	{
-		l = b;
-	}
+	{l = b; }
 	if (l + 1 > size_r)
-	{
-		return (0);
-	}
+	{return (0); }
 	r[l] = '\0';
 	for (k = l - 1; k >= 0; k--)
-	{
-		a--;
-		b--;
+	{a--, b--;
 		if (a >= 0)
-		{
-			f = n1[a] - '0';
-		}
-		else 
-		{
-			f = 0;
-		}
-		if (b >= 0)
-		{
-			s = n2[b] - '0';
-		}
+		{f = n1[a] - '0'; }
 		else
-		{
-			s = 0;
-		}
+		{f = 0; }
+		if (b >= 0)
+		{s = n2[b] - '0'; }
+		else
+		{s = 0; }
 		r[k] = (f + s + d) % 10 + '0';
-		d = (f + s + d) / 10;
-	}
+		d = (f + s + d) / 10; }
 	if (d == 1)
-	{
-		r[l + 1] = '\0';
+	{r[l + 1] = '\0';
 		if (l + 2 > size_r)
-		{
-			return (0);
-		}
+		{return (0); }
 		while (l-- >= 0)
-		{
-			r[l + 1] = r[l];
-		}
-	r[0] = d + '0';
-	}
+		{r[l + 1] = r[l]; }
+	r[0] = d + '0'; }
 	return (r);
 }
