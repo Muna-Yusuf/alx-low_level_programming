8e37ee95604266ff1bfc5badba7e8b82c62f8f80
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-24 09:56:30 -0700

    the6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,8 +8,19 @@
 
 char *cap_string(char *str)
 {
-	int n = 1;
-	char str2;
+	char *str2 = str;
+	int j = 0;
 
 
+	while (str[j])
+	{
+		while (!(str[j] >= 'a' && str[j] <= 'z'))
+		{
+			j++;
+		}
+		if (str[j - 1] == ' ' || str[j - 1] == '\t' || str[j - 1] == '\n' || str[j - 1] == ';' || str[j - 1] == ',' || str[j - 1] == '.' || str[j - 1] == '!' || str[j - 1] == '?' || str[j - 1] == '"' || str[j - 1] == '(' || str[j - 1] == ')' || str[j - 1] == '{' || str[j - 1] == '}' || j == 0)
+			str[j] -= 32;
+		j++;
+	}
+	return (str2);
 }
