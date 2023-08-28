6419f01cdab2db85a80f1df3651d8db9392a09b3
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-28 10:46:09 -0700

    ....

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr -  a function that locates a substring.
+ * @haystack: string
+ * @needle: will compare it with s to find matches
+ * Return:the matched characters or NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j;
+
+	while (needle[i] != '\0')
+	{
+		i++;
+	}
+	while (*haystack != '\0')
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (haystack[i] != needle[i])
+			{
+				break;
+			}
+		}
+		if (j != i)
+		{
+			haystack++;
+		}
+		else
+			return (haystack);
+	}
+	return ('\0');
+
+}
