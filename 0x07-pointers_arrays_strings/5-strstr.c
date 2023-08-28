08b2afb13842d29deecd0b9fb74216df64eadaff
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-28 11:05:42 -0700

    ...

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,12 +1,11 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
  * _strstr -  a function that locates a substring.
  * @haystack: string
  * @needle: will compare it with s to find matches
  * Return:the matched characters or NULL
  */
-
 char *_strstr(char *haystack, char *needle)
 {
 	int i = 0;
