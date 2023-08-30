0b3e9d54291559ab099936c9fa17b1969278031b
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-30 07:47:22 -0700

    ...

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,34 +1,34 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function  returns the natural square root of a number
+ * _sqrt_recursion - function  returns the natural square root of a number.
  * @n: number
- * Return: if n = 0 than 
+ * Return: if n = 0 than -1
  */
 
-int sq(int num, int x);
 int _sqrt_recursion(int n)
 {
 return (sq(n, 1));
 }
 
-/**
- * sq - function to calculate (square)
- * @num: for findig square
- * @x: square root
- * Return: number
- */
+int sq(int n, int x);
 
-int sq(int num, int x)
+/**
+ * sq - function to calculate (square).
+ * @x:square root.
+ * @n: to find square.
+ * Return: square root
+ */
+int sq(int n, int x)
 {
-	if (x * x == num)
+	if (x * x == n)
 	{
 		return (x);
 	}
-	else if (x * x < num)
+	else if (x * x < n)
 	{
-		return (sq(num, x + 1));
+		return (sq(n, x + 1));
 	}
-	else 
+	else
 		return (-1);
 }
