3bf4a4ad2dc39f8d52f0a991b572efc1c29244e7
muna-yusuf <muna.y.a.jim@gmail.com>
2023-08-29 23:14:42 -0700

    ...

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function that returns the value of x raised to the power of y
+ * _pow_recursion - function returns the value of x raised to the power of y
  * @x: the base
  * @y: power
  * Return: if y =  0 (-1),else x
