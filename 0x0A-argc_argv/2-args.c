7725de37b433ab726fe9e7a1e23971caeb5b126c
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-01 11:52:23 -0700

    ...

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - a program that prints all arguments it receives
+ * @argc: int
+ * @argv: list
+ * Return:0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	(void)argc;
+
+return (0);
+}
