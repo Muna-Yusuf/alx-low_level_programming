aec6bca3ea44f886ddde68163b0b1fd8ef7700ad
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-07 13:46:09 -0700

    ...

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -16,9 +16,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (ptr);
 	}
-	x = malloc(new_size);
 	if (ptr == NULL)
 	{
+		x = malloc(new_size);
 		if (x == NULL)
 		{
 			return (NULL);
@@ -33,6 +33,8 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		}
 	}
+	x = malloc(new_size);
+
 	if (x == NULL)
 	{
 		return (0);
