d44ea8fa51c95bfdb7e57f90789f57c7186d7078
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-11 10:11:09 -0700

    ...

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Define a new type.
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
