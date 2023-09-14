1d39dd724ca490175c02aff8b20f201b4f1e4759
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-14 11:12:51 -0700

    ...

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
-	if (n == 0)
-	{
-		printf("\n");
-	}
 	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
