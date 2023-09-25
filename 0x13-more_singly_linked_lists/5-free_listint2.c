ebbb8b9930546a89a909665c9e8635c86bb9d79c
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-25 11:30:58 -0700

    ...

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a listint_t list.
+ * @head: singly linked lists.
+ * Return: void.
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *h;
+
+	h = *head;
+	while (h)
+	{
+		tmp = h;
+		h = h->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
