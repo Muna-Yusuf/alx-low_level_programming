a828bb75c2c605cffa518bfdab3fe2e48fe5924a
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-28 13:14:56 -0700

    ...

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,23 +3,19 @@
 /**
  * set_bit - function sets the value of a bit to 1 at a given index.
  * @n: number.
- * index:the index, starting from 0 of the bit you want to set.
+ * @index:the index, starting from 0 of the bit you want to set.
  *
  * Return:1 if it worked, or -1 if an error occurred.
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int numb_bin = (sizeof(n) * 8), i;
+	unsigned long int numb_bin = (sizeof(n) * 8);
 
 	if (index > numb_bin)
 	{
 		return (-1);
 	}
-	for (i = 1; index > 0; index--)
-	{
-		i *= 2;
-	}
-	*n += i;
+	(*n) = ((1 << index) | (*n));
 	return (1);
 }
