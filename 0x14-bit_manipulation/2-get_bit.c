8dadee3936cf2d0f7121ad5fee37f6448c763f48
muna-yusuf <muna.y.a.jim@gmail.com>
2023-09-28 12:55:16 -0700

    ...

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@
  * @n:number.
  * @index:is the index, starting from 0 of the bit you want to get.
  *
- * Return:the value of the bit at index index or 
+ * Return:the value of the bit at index index or
  * -1 if an error occured.
  */
 
