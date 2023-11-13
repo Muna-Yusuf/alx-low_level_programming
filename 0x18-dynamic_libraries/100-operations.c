d13b9069c1e417ae0ac0f6fcaca55876e4616bf7
Muna-Yusuf <muna.y.a.jim@gmail.com>
2023-11-13 08:52:01 -0800

    ...

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - function adds numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: a + b.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
