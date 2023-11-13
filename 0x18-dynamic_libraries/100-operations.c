e5e4d76fd0d582d1519980e8c22465886bc88242
Muna-Yusuf <muna.y.a.jim@gmail.com>
2023-11-13 09:28:46 -0800

    ...

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * add - function adds numbers.
@@ -11,4 +10,57 @@
 int add(int a, int b)
 {
 	return (a + b);
+}
+
+/**
+ * sub - function sub numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: a - b.
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function mul numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: a * b.
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function div numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: a / b.
+ */
+
+int div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Can't div by zero \n");
+		return (0);
+	}
+	return (a / b);
+}
+
+/**
+ * mod - function mod numbers.
+ * @a: number 1.
+ * @b: number 2.
+ * Return: a + b.
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
 }
