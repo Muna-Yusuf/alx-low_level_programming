46b246b129444801503c952431f2ba6a5531f87c
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-11-30 10:42:48 -0800

    ...

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,29 +8,22 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new;
-	unsigned long int i;
+	hash_table_t *table;
 
 	if (size == 0)
+		return (NULL);
+
+	table = calloc(1, sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = calloc(size, sizeof(hash_node_t *));
+	if (table->array == NULL)
 	{
+		free(table);
 		return (NULL);
 	}
-	new = malloc(sizeof(hash_table_t));
-	if (new == NULL)
-	{
-		return (NULL);
-	}
-	new->size = size;
-	new->array = malloc(size * sizeof(hash_node_t *));
-	if (new->array == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	for (i = 0; i < new->size; i++)
-	{
-		new->array[i] = NULL;
-	}
-	return (new);
+	return (table);
 }
 
