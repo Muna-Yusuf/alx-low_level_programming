1c71c6424aa7bd4e1ba2dc3d08db1ddfa9fa7078
Muna-Yusuf <Muna.y.a.jim@gmail.com>
2023-12-01 09:04:26 -0800

    ...'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,36 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - Creates a hash table.
- * @size: The size of the array.
+ * hash_table_create - function that creates a hash table.
+ * @size: is the size of the array.
  *
- * Return: If an error occurs - NULL.
- *         Otherwise - a pointer to the new hash table.
+ * Return: a pointer to the newly created hash table , or Null.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t  *htable;
+	hash_table_t *new;
 	unsigned long int i;
 
-	htable = malloc(sizeof(hash_table_t));
-	if (htable == NULL)
+	if (size == 0)
+	{
 		return (NULL);
+	}
 
-	htable->size = size;
-	htable->array = malloc(sizeof(hash_node_t *) * size);
-	if (htable->array == NULL)
+	new = malloc(sizeof(hash_table_t));
+	if (new == NULL)
+	{
 		return (NULL);
-	for (i = 0; i < size; i++)
-		htable->array[i] = NULL;
-
-	return (htable);
+	}
+	new->size = size;
+	new->array = malloc(size * sizeof(hash_node_t *));
+	if (new->array == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	for (i = 0; i < new->size; i++)
+	{
+		new->array[i] = NULL;
+	}
+	return (new);
 }
--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,41 +11,44 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *current, *tmp;
-	unsigned long int i;
+	hash_node_t *current, *new;
+	unsigned long int index;
 
 	if (!ht || !key || !value)
 	{
-		return (NULL);
+		return (0);
 	}
-	index = hash_djb2((unsigned char * )key) % ht->size;
+	index = hash_djb2((unsigned char *)key) % ht->size;
 	current = ht->array[index];
 	while (current)
 	{
-		if ((strcmp(current->key), key) == 0)
+		if (strcmp(current->key, key) == 0)
 		{
 			free(current->value);
 			current->value = strdup(value);
 			if (!current->value)
-			{
 				return (0);
-			}
 			return (1);
 		}
-		current = current->next
+		current = current->next;
 	}
-
-	new = mallco(sizeof(hash_node_t));
+	new = malloc(sizeof(hash_node_t));
 	if (!new)
-	{
 		return (0);
-	}
-	new->key =strdup(key);
+	new->key = strdup(key);
 	if (!new->key)
 	{
 		free(new);
 		return (0);
 	}
-	new->value =strdup(value);
-	if (
+	new->value = strdup(value);
+	if (!new->value)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	new->next = ht->array[index];
+	ht->array[index] = new;
+	return (1);
 }
